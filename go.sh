5541b78f91661d8b6ab16be689b1ced603309f20
Rob Parrett <robparrett@gmail.com>
2025-03-04 05:53:55 -0700

    Tweak automation

--- go.sh
+++ go.sh
@@ -3,14 +3,15 @@
 # Configuration variables
 
 SOLVER_SIZE=200
-NUM_ITERATIONS=5000
+NUM_ITERATIONS=4000
 VISUALIZE=false
 CSV_FILE=polylite-abs-owned.csv
 
 source venv/bin/activate
 
 # Get and shuffle jpg/jpeg images from "in" dir
-image_list=($(ls in/*.{jpg,jpeg} 2>/dev/null | shuf))
+#image_list=($(ls in/*.{jpg,jpeg} 2>/dev/null | shuf))
+image_list=($(ls in/*.{jpg,jpeg} 2>/dev/null))
 
 # Process images in shuffled order
 for img in "${image_list[@]}"; do
@@ -28,6 +29,7 @@ for img in "${image_list[@]}"; do
         --output_folder="out/$name"
         --iterations=$NUM_ITERATIONS
         --solver_size=$SOLVER_SIZE
+        --random_seed=0
     )
 
     # Add visualization flag if enabled
