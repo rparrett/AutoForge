ad8e2391c0aabe516c0834c1ec28a9b81b0e9e3e
Rob Parrett <robparrett@gmail.com>
2025-03-13 05:10:14 -0700

    Report tweaks

--- lowest_loss.py
+++ lowest_loss.py
@@ -33,17 +33,21 @@ def main():
 def generate_html_report(dir_losses):
     html_content = """
     <html>
-    <head><title>Loss Report</title></head>
+    <head>
+        <title>Loss Report</title>
+        <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">
+        <link rel="stylesheet" href="https://unpkg.com/normalize.css@8.0.1/normalize.css" type="text/css">
+    </head>
     <body>
     <h1>Loss Report</h1>
-    <table border="1">
+    <table>
         <tr><th>Directory</th><th>Loss</th><th>Model Image</th></tr>
     """
 
     for name, loss in dir_losses:
         dir_path = os.path.abspath(Path('out') / name)
         img_path = Path('out') / name / 'final_model.png'
-        img_tag = f'<img src="{img_path}" alt="Model Image" width="100">' if img_path.exists() else 'No Image'
+        img_tag = f'<img src="{img_path}" alt="Model Image">' if img_path.exists() else 'No Image'
         html_content += f"<tr><td><a href='file://{dir_path}'>{name}</a></td><td>{loss:.6f}</td><td>{img_tag}</td></tr>"
 
     html_content += """
